69c950e6a35624c060dc96462ee5f0b9b6c9cca2
faezehM84 <bureikou130613@gmail.com>
2024-05-11 04:49:15 -0700

    Add files via upload

---
+++ score.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "global.hpp"
+
+class Score{
+public:
+    Score();
+    ~Score();
+    void render(RenderWindow* window); 
+
+private:
+    Texture scoreTexture ;
+    Sprite scoreSprite ;
+    Text scoreText;
+    Font scoreFont;
+
+};
