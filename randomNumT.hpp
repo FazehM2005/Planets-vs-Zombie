f5ed7cde3d73784c11e8156c417b2c4cb26a0dfc
faezehM84 <bureikou130613@gmail.com>
2024-05-11 19:21:21 +0900

    Delete randomNumT.hpp

--- randomNumT.hpp
+++
@@ -1,9 +0,0 @@
-#include "global.hpp"
-
-int generateRandomNumber(int min, int max)
-{
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<int> dist(min,max);
-    return dist(gen);
-}
