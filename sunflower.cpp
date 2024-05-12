f827719a54371691653ac31f1728a49411217a45
faezehM84 <bureikou130613@gmail.com>
2024-05-12 00:33:33 -0700

    Add files via upload

---
+++ sunflower.cpp
@@ -0,0 +1,60 @@
+#include "sunflower.hpp"
+
+Sunflower::Sunflower(int x, int y)
+{
+    pos = Vector2f(x,y);
+    sunflowertexture.loadFromFile("sunflowerplant.png");
+    sunflowersprite.setTexture(sunflowertexture);
+    sunflowersprite.setScale(0.4 ,0.4);
+    rect.width = 190;
+    rect.height = 207;
+    rect.top = 0;
+    rect.left = 0;
+    sunflowersprite.setTextureRect(rect);
+}
+
+
+Sunflower::~Sunflower()
+{
+}
+
+void Sunflower::render(RenderWindow *window)
+{
+    window->draw(sunflowersprite);
+}
+
+void Sunflower::update(Vector2i pos)
+{
+    if(is_tagged){
+        Vector2f target(static_cast<float>(pos.x) - sunflowersprite.getTextureRect().width, static_cast<float>(pos.y) - sunflowersprite.getTextureRect().height);
+        sunflowersprite.setPosition(target);
+    }
+    fix_position();
+}
+
+void Sunflower::handle_mouse_press(Vector2f mousePos)
+{
+    Vector2f spritePos = sunflowersprite.getPosition();
+    Vector2f spriteSize = {sunflowersprite.getTextureRect().width * 2, sunflowersprite.getTextureRect().height * 2};
+    if (mousePos.x >= spritePos.x && mousePos.x <= spritePos.x + spriteSize.x &&
+        mousePos.y >= spritePos.y && mousePos.y <= spritePos.y + spriteSize.y)
+    {
+        is_tagged = true;
+    }
+}
+
+void Sunflower::handle_mouse_release(Vector2f pos)
+{
+    is_tagged = false;
+    return;
+}
+
+void Sunflower::fix_position()
+{
+    FloatRect rect = sunflowersprite.getGlobalBounds();
+    rect.top = max(0.f, rect.top);
+    rect.top = min(rect.top, HEIGHT - rect.height);
+    rect.left = max(0.f, rect.left);
+    rect.left = min(rect.left, WIDTH - rect.width);
+    sunflowersprite.setPosition(rect.left, rect.top);
+}
